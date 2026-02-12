0a7f81b259da4c2eb66a676689750b34c5b543a7
Romer Girginer <girgineromer3019@gmail.com>
2026-02-12 12:03:27 +0100

    React
    Ik heb basic van react geleerd

--- ReactLearning/src/App.jsx
+++ ReactLearning/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './Login'
 
 function App() {
-  const names = ["ALI", "AHMED", "MOHAMED"];
+  
 
   return (
     <div>
-      Girginer
+      <Login />
     </div>
   )
 }
