daa5fe9c41fa3e591074a2cffb3388191daf41b0
Romer Girginer <girgineromer3019@gmail.com>
2026-02-16 11:02:25 +0100

    React
    Ik ben bezig met React te leren

--- ReactLearning/src/App.jsx
+++ ReactLearning/src/App.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import './App.css';
 import Product from './Product';
+import { useState } from "react";
 
 function App() {
+
+//useState = bir hooks (verileri yonetmek icin kullanilir.)
+const [name, setname] = useState('Omer');
+const [name1, setname1] = useState ('Ali');
+const [name2, setname2] = useState ('Veli');
+const [name3, setname3] = useState ('Ayse');
+
+
   return (
     <div>
+      {name}
+      <hr />
+      <button onClick={() => setname('Ahmet')}>Change Name</button>
       <Product name="T-shirt" price={20} />
       <Product name="Jeans" price={40} />
       <Product name="Sneakers" price={60} />
